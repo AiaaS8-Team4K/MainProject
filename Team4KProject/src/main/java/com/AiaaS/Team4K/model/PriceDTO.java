226f8b08befe4608627618b1c8c380132a68a7a6
김민준 <kimmj9703@gmail.com>
2024-08-22 14:26:57 +0900

    개인 페이지 수정 및 컨트롤러 메서드 추가, 푸터 글자 크기 축소
    개인 페이지 수정 및 컨트롤러 메서드 추가, 푸터 글자 크기 축소

---
+++ Team4KProject/src/main/java/com/AiaaS/Team4K/model/PriceDTO.java
@@ -0,0 +1,35 @@
+package com.AiaaS.Team4K.model;
+
+public class PriceDTO {
+	private double oilPrice;
+    private double coalPrice;
+    private double LNGPrice;
+    private double uranPrice;
+    
+	public double getOilPrice() {
+		return oilPrice;
+	}
+	public void setOilPrice(double oilPrice) {
+		this.oilPrice = oilPrice;
+	}
+	public double getCoalPrice() {
+		return coalPrice;
+	}
+	public void setCoalPrice(double coalPrice) {
+		this.coalPrice = coalPrice;
+	}
+	public double getLNGPrice() {
+		return LNGPrice;
+	}
+	public void setLNGPrice(double lNGPrice) {
+		LNGPrice = lNGPrice;
+	}
+	public double getUranPrice() {
+		return uranPrice;
+	}
+	public void setUranPrice(double uranPrice) {
+		this.uranPrice = uranPrice;
+	}
+    
+    
+}
